b402600831eb93619ca04a3c903c5ae80b53e1b3
Gabriel Leitão <xgabriel.rf@gmail.com>
2022-03-04 03:25:15 -0300

    add steps

--- gitlab-instance/provision.sh
+++ gitlab-instance/provision.sh
@@ -1,5 +1,13 @@
-#!/usr/bin/env bash
-echo "Installing Apache and setting it up..."
-yum install -y httpd >/dev/null 2>&1
-cp -r /vagrant/html/* /var/www/html/
-service httpd start
+#!/bin/bash
+echo "Installing default packages..."
+sudo apt-get update
+sudo apt-get install -y curl openssh-server ca-certificates tzdata perl net-tools
+echo "Installing postfix..."
+sudo apt-get install -y postfix
+echo "Installing gitlab..."
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+sudo apt-get update
+sudo apt-get install gitlab-ce
+sudo export EXTERNAL_URL=http://$(hostname)
+sudo gitlab-ctl reconfigure
+sudo cat /etc/gitlab/initial_root_password
